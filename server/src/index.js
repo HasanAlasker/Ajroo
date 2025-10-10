845bfd066965b17da49da76b783933184c70114c
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-10 14:22:35 +0300

    mongo practice

--- server/src/index.js
+++ server/src/index.js
@@ -50,14 +50,44 @@ const createCourse = async () => {
   }
 };
 
-createCourse()
+// createCourse();
 
 const getCourses = async () => {
+  // comparison operators:
+  // eq (equal to)
+  // ne (not equal to)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  // lte (less than or equal to)
+  // in
+  // nin (not in)
+
+  // logical operators
+  // or     ex: courseModle.find().or([ {author: "Hasan Alasker"}, {isPublished: true} ])
+  // and    ex: // // .and([ {author: "Hasan Alasker"}, {isPublished: true} ])
+
+  // regular expressions (example based)
+  // .find({ author : /^Hasan/ })  // find string that STARTS with Hasan, it will only find the first one
+  // .find({ author : /^Hasan/g })  // find string that STARTS with Hasan, it will find all
+  // .find({ author : /^Hasan/i })  // make it not case sensitive (it can find hasan also)
+  // .find({ author : /^Hasan/gi })  // make it not case sensitive and find all of them
+  // .find({ author : /Alasker$/ })  // find string that ENDS with Alasker, make it not case sensitive by adding i
+  // .find( { author : /.*Alasker*./ }) find strings that CONTAIN Alasker, add i if you want
+
+  // to devide big data into chunks for performance we use PAGINATION, ex: load 10 posts at a time.
+  const pageNumber = 1;
+  const pageSize = 10; // you usually get this form an API query api/courses?pageNumber=2&pageSize=10
+  // use .skip((pageNumber-1)*pageSize) and .limit(pageSize) for it
+
   try {
-    const courses = await CourseModel.find({ isPublished: true })
-      .limit(10)
-      .sort({ name: 1 })
-      .select({ name: true, _id: false });
+    const courses = await CourseModel.find()
+      .or([{ author: /.*omar*./i }, { name: /an*/ }, { price: { $gte: 15 } }])
+      .skip((pageNumber - 1) * pageSize)
+      .limit(pageSize)
+      .sort({ price: 1 })
+      .select({ name: true, _id: false, price: true, author: true });
+    // .countDocuments() // just gives you the number of documents (also based on your filtiring)
     console.log(courses);
   } catch (err) {
     console.log(err);
