c7cc393fde3a122d694d970110a8b9f771dc7fab
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-24 18:50:44 +0300

    mark as returned route

--- server/src/routers/borrows.js
+++ server/src/routers/borrows.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import auth from "../middleware/auth.js";
 import admin from "../middleware/admin.js";
 import BorrowModel from "../models/borrowsModel.js";
+import { updateBorrowValidation } from "../validation/borrowValidation.js";
+import validate from "../middleware/joiValidation.js";
 
 const router = express.Router();
 
@@ -29,7 +31,6 @@ router.get("/given", auth, async (req, res) => {
       return res.status(404).send("You haven't given any items");
 
     return res.status(200).send(givenItems);
-
   } catch (err) {
     return res.status(500).send(err.message);
   }
@@ -44,13 +45,47 @@ router.get("/taken", auth, async (req, res) => {
       return res.status(404).send("You haven't taken any items");
 
     return res.status(200).send(takenItems);
-    
   } catch (err) {
     return res.status(500).send(err.message);
   }
 });
 
 // borrower mark as returned (PUT), must be confirmed by owner
+
+router.put(
+  "/mark-returned/:id",
+  [auth, validate(updateBorrowValidation)],
+  async (req, res) => {
+    try {
+      const borrowId = req.params.id;
+
+      if (!borrowId || !mongoose.Types.ObjectId.isValid(borrowId)) {
+        return res.status(400).send("Invalid borrow ID");
+      }
+
+      const borrowedItem = await BorrowModel.findById(borrowId);
+      if (!borrowedItem) return res.status(404).send("Item not found");
+      
+      if (borrowedItem.status === "completed")
+        return res.status(400).send("You already marked item as returned");
+
+      if (req.user._id.toString() !== borrowedItem.borrower.toString())
+        return res.status(403).send("You can't return and item you don't have");
+
+      const updatedBorrow = await BorrowModel.findByIdAndUpdate(
+        borrowId,
+        {
+          status: "completed",
+        },
+        { new: true, runValidators: true }
+      );
+
+      return res.status(200).send(updatedBorrow);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  }
+);
 
 // owner mark as confirmed (DELETE)
 
