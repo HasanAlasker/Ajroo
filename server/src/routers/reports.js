e60fc97f8b75eec8edebc2612ab4bf5b09ecb8a1
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-26 17:40:25 +0300

    getting route ready

---
+++ server/src/routers/reports.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router = express.Router();
+
+// create report
+
+// delete report
+
+// block user
+
+// delete post (i have a soft-delete endpoint in Posts.js)
+
+// add strike
+
+export default router;
