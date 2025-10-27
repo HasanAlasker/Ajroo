cf2108e920ff8ff2d96cc866a09c9c28016fd5c0
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-27 09:26:53 +0300

    report user

--- server/src/routers/reports.js
+++ server/src/routers/reports.js
@@ -6,6 +6,7 @@ import { createReport } from "../validation/reportValidation.js";
 import ReportModel from "../models/reportModel.js";
 import PostModel from "../models/postsModel.js";
 import _ from "lodash";
+import UserModel from "../models/usersModel.js";
 
 const router = express.Router();
 
@@ -17,25 +18,28 @@ router.post("/post/:id", [auth, validate(createReport)], async (req, res) => {
     const { reason } = req.body;
 
     if (!postId || !mongoose.Types.ObjectId.isValid(postId)) {
-      return res.status(400).send("Invalid borrow ID");
+      return res.status(400).send("Invalid post ID");
     }
 
     const reportedPost = await PostModel.findById(postId);
 
     if (!reportedPost) return res.status(404).send("Post not found");
 
-    const existingReport = await ReportModel.findOne({reporter: req.user._id, reportedPost: postId})
-    if(existingReport) return res.status(400).send("You already reported this post")
+    const existingReport = await ReportModel.findOne({
+      reporter: req.user._id,
+      reportedPost: postId,
+    });
+    if (existingReport)
+      return res.status(400).send("You already reported this post");
 
     const newReport = new ReportModel({
       reporter: req.user._id,
       reportedPost: postId,
       reason,
     });
-    await newReport.save()
+    await newReport.save();
 
-    return res.status(201).send(newReport)
-
+    return res.status(201).send(newReport);
   } catch (err) {
     return res.status(500).send(err.message);
   }
@@ -43,7 +47,38 @@ router.post("/post/:id", [auth, validate(createReport)], async (req, res) => {
 
 // report user
 
-router.post("user/:id", [auth, validate(createReport)], async (req, res) => {});
+router.post("user/:id", [auth, validate(createReport)], async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { reason } = req.body;
+
+    if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).send("Invalid user ID");
+    }
+
+    const reportedUser = await UserModel.findById(userId);
+
+    if (!reportedUser) return res.status(404).send("User not found");
+
+    const existingReport = await ReportModel.findOne({
+      reporter: req.user._id,
+      reportedUser: userId,
+    });
+    if (existingReport)
+      return res.status(400).send("You already reported this user");
+
+    const newReport = new ReportModel({
+      reporter: req.user._id,
+      reportedUser: userId,
+      reason,
+    });
+    await newReport.save();
+
+    return res.status(201).send(newReport);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
 
 // delete report (admin)
 
