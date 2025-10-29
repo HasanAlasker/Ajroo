6fb723fce4fb94baffd968b1443add77ef0c5092
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-29 11:08:30 +0300

    example cloudinary usage in upload.js

--- client/api/upload.js
+++ client/api/upload.js
@@ -30,3 +30,34 @@ export const uploadImage = async (imageUri) => {
     throw error;
   }
 };
+
+
+// In your Post creation screen
+// import { uploadImage } from '../api/upload';
+// import { addPost } from '../api/posts';
+
+// const handleCreatePost = async () => {
+//   try {
+//     // 1. First upload the image and get URL
+//     const imageUrl = await uploadImage(selectedImage.uri);
+    
+//     // 2. Then create post with the URL
+//     const postData = {
+//       name: "Drill",
+//       category: "tools",
+//       image: imageUrl, // Store the Cloudinary URL
+//       pricePerDay: 50,
+//       city: "Amman",
+//       area: "Downtown",
+//       condition: "good"
+//     };
+    
+//     const response = await addPost(postData);
+    
+//     if (response.ok) {
+//       // Success!
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
