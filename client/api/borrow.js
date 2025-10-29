1e878b2492e0829dd3e2f90f2a1de7f1276f06b0
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-29 10:34:30 +0300

    borrows endpoint

---
+++ client/api/borrow.js
@@ -0,0 +1,18 @@
+import { apiClient } from "./client";
+
+const endPoint = "/api/borrows"
+
+export const getAllBorrows = () => apiClient.get(endPoint)
+
+export const givenItems = () => apiClient.get(`${endPoint}/given`)
+
+export const takenItems = () => apiClient.get(`${endPoint}/taken`)
+
+export const markReturned = (id) => apiClient.put(`${endPoint}/mark-returned/${id}`)
+
+export const confirmReturn = (id) => apiClient.delete(`${endPoint}/confirm-return/${id}`)
+
+export const rejectConfirmation = (id) => apiClient.put(`${endPoint}/reject-confirmation/${id}`)
+
+export const itemsMarkedAsReturned = () => apiClient.get(`${endPoint}/marked-as-returned`)
+
