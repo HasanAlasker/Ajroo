5ad65a2a7b769eed56fd61703733d44c25c05d67
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-29 10:26:59 +0300

    request endPoints

--- client/api/request.js
+++ client/api/request.js
@@ -1,4 +1,15 @@
 import { apiClient } from "./client";
 
-const endPoint = '/api/requests'
+const endPoint = "/api/requests";
 
+export const createRequest = (id, data) =>
+  apiClient.post(`${endPoint}/${id}`, data);
+
+export const deleteRequest = (id) => apiClient.delete(`${endPoint}/${id}`); // cancel/ reject
+
+export const confirmRequest = (id, data) =>
+  apiClient.post(`${endPoint}/borrow/${id}`, data);
+
+export const gotRequests = () => apiClient.get(`/got`);
+
+export const sentRequests = () => apiClient.get(`/sent`);
