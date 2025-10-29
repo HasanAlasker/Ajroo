f3b05f768f3cfd69adabaf27f7df9ef107b83493
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-29 08:47:48 +0300

    useApi hook

---
+++ client/hooks/useApi.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default useApi = (apiFunc) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const request = async (...arg) => {
+    setLoading(true);
+    const response = await apiFunc(...arg);
+    setLoading(false);
+
+    if (!response.ok) return setError(true);
+
+    setError(false);
+    setData(response.data);
+  };
+
+  return {error, loading, data, request}
+};
