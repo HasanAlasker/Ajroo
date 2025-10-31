d436094e31e83d6321bbf09da6dc0b43d5bf873f
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-31 20:00:49 +0300

    fixed image upload problem , no webp yet

--- client/api/report.js
+++ client/api/report.js
@@ -4,13 +4,13 @@ const endPoint = "/api/reports";
 
 export const reportedPosts = () => apiClient.get(`${endPoint}/posts`) // ✅
 
-export const reportedUsers = () => apiClient.get(`${endPoint}/users`)
+export const reportedUsers = () => apiClient.get(`${endPoint}/users`) 
 
 export const reportPost = (id, data) =>
   apiClient.post(`${endPoint}/post/${id}`, data);
 
 export const reportUser = (id, data) =>
-  apiClient.post(`${endPoint}/user/${id}`, data);
+  apiClient.post(`${endPoint}/user/${id}`, data); // ✅
 
 export const deleteReport = (id) =>  
   apiClient.delete(`${endPoint}/delete/${id}`);  // ✅
