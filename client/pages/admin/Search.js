04856b7f35b4b3619cbb607bab3f9e06043662aa
Hasan Alasker <hasanalasker2004@gmail.com>
2025-10-31 13:33:33 +0300

    render all posts in admin search, TODO: fix pributton for admin

--- client/pages/admin/Search.js
+++ client/pages/admin/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import WelcomeCard from "../../components/WelcomeCard";
 import SafeScreen from "../../components/general/SafeScreen";
@@ -8,17 +8,31 @@ import SearchBar from "../../components/general/SearchBar";
 import PostRenderer from "../../components/PostRenderer";
 import useApi from "../../hooks/useApi";
 import { getPosts } from "../../api/post";
+import { useUser } from "../../config/UserContext";
 
 function Search(props) {
+  const {user} = useUser()
   const {data: posts, request: fetchPosts, error, loading} = useApi(getPosts)
+  const [refresh, setRefresh] = useState(false)
 
   useEffect(()=>{
-    
+    fetchPosts()
   },[])
+
+  const handleRefresh = () => {
+    setRefresh(true)
+    fetchPosts()
+    setRefresh(false)
+  }
 
   return (
     <SafeScreen>
-      <PostRenderer>
+      <PostRenderer
+        onRefresh={handleRefresh}
+        refreshing={refresh}
+        fetchedPosts={posts}
+        currentUserId={user.id}
+      >
         <SearchBar />
       </PostRenderer>
       <Navbar></Navbar>
