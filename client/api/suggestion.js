0b1178bd7756445c3ad93c851f6d0ddbfb3af9df
Hasan Alasker <hasanalasker2004@gmail.com>
2025-11-07 11:33:04 +0300

    api endpoints

--- client/api/suggestion.js
+++ client/api/suggestion.js
@@ -2,7 +2,7 @@ import { apiClient } from "./client";
 
 const endpoint = "/api/suggestions";
 
-export const makeSuggestion = (data) => apiClient.post(endpoint);
+export const makeSuggestion = (data) => apiClient.post(endpoint, data);
 
 export const getSuggestions = () => apiClient.post(endpoint);
 
