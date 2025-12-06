c00a55419fd5ed3e3213b0b92e5e730c6b819eaa
Hasan Alasker <hasanalasker2004@gmail.com>
2025-12-06 13:54:21 +0300

    added all routes for news

--- server/src/routers/news.js
+++ server/src/routers/news.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 import NewsModel from "../models/newsModel.js";
 import admin from "../middleware/admin.js";
 import auth from "../middleware/auth.js";
-import { createNewsValidation } from "../validation/newsValidation.js";
+import {
+  createNewsValidation,
+  editNewsValidation,
+} from "../validation/newsValidation.js";
 import validate from "../middleware/joiValidation.js";
 
 const router = express.Router();
@@ -29,6 +32,28 @@ router.post(
 
 // edit news (admin)
 
+router.put(
+  "/edit/:id"[(auth, admin, validate(editNewsValidation))],
+  async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send("Invalid news ID");
+    }
+
+    const editedNews = await NewsModel.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!editedNews)
+      return res.status(400).send("Didn't update, something went wrong!");
+
+    return res.status(200).send(editedNews);
+  }
+);
+
 // delete news (admin)
 
 router.delete("/delete/:id", [auth, admin], async (req, res) => {
@@ -47,8 +72,54 @@ router.delete("/delete/:id", [auth, admin], async (req, res) => {
 
 // deactivate news (admin)
 
-// get active new banner (auth users)
+router.put("/deactivate/:id", [auth, admin], async (req, res) => {
+  const id = req.params.id;
+
+  if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send("Invalid news ID");
+  }
+
+  const deactivatedNews = await NewsModel.findByIdAndUpdate(
+    id,
+    { isActive: false },
+    { new: true }
+  );
+
+  return res.status(200).send(deactivatedNews);
+});
+
+// deactivate news (admin)
+
+router.put("/activate/:id", [auth, admin], async (req, res) => {
+  const id = req.params.id;
+
+  if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send("Invalid news ID");
+  }
+
+  const activatedNews = await NewsModel.findByIdAndUpdate(
+    id,
+    { isActive: true },
+    { new: true }
+  );
+
+  return res.status(200).send(activatedNews);
+});
 
 // get news log (auth/ admin)
+
+router.get("/", auth, async (req, res) => {
+  const allNews = await NewsModel.find();
+  if (!allNews) return res.status(404).send("No news found");
+  return res.status(200).send(allNews);
+});
+
+// get active new banner (auth users)
+
+router.get("/active", auth, async (req, res) => {
+  const activeNews = await NewsModel.findOne({ isActive: true });
+  if (!activeNews) return res.status(404).send("No active news found");
+  return res.status(200).send(activeNews);
+});
 
 export default router;
