85e8ab1d3f384bf72cec03a18be547990871ba9d
Hasan Alasker <hasanalasker2004@gmail.com>
2025-12-10 14:03:59 +0300

    ad

---
+++ client/components/post_releated/AdPost.js
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  Pressable,
+} from "react-native";
+import PostComponent from "./PostComponent";
+import TopOfPost from "./TopOfPost";
+import { useTheme } from "../../config/ThemeContext";
+import useThemedStyles from "../../hooks/useThemedStyles";
+import AppText from "../../config/AppText";
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
+import { openURL } from "../../functions/openURL";
+import { useAlert } from "../../config/AlertContext";
+
+function AdPost({ userId, userPic, userName, userSub, image, link }) {
+  const styles = useThemedStyles(getStyles);
+  const { theme } = useTheme();
+  const {showAlert} = useAlert()
+
+  return (
+    <PostComponent style={styles.container}>
+      <View style={styles.topPad}>
+        <TopOfPost
+          date={"Sponsored"}
+          image={userPic}
+          isMine={false}
+          name={userName}
+          subscriptionType={userSub}
+          userId={userId}
+          hideThree
+        />
+      </View>
+      <Image source={{ uri: image }} style={styles.img} />
+      <Pressable style={styles.btn} onPress={()=>openURL(link, showAlert)}>
+        <AppText style={styles.text}>Learn More</AppText>
+        <MaterialCommunityIcons
+          name="chevron-right-circle-outline"
+          size={23}
+          color={theme.always_white}
+          style={{ paddingTop: 3 }}
+        />
+      </Pressable>
+    </PostComponent>
+  );
+}
+
+const getStyles = (theme) =>
+  StyleSheet.create({
+    container: {
+      paddingHorizontal: 0,
+      paddingVertical: 0,
+      rowGap: 0,
+    },
+    topPad: {
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+    },
+    btn: {
+      paddingHorizontal: 20,
+      paddingVertical: 12,
+      backgroundColor: theme.purple,
+      flex: 1,
+      borderBottomLeftRadius: 15,
+      borderBottomRightRadius: 15,
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 5,
+    },
+    img: {
+      width: "100%",
+      aspectRatio: 1,
+      backgroundColor: theme.post,
+    },
+    text: {
+      color: theme.always_white,
+      fontSize: 22,
+    },
+  });
+
+export default AdPost;
